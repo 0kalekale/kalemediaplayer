48e6fbb9532234b07df405cc51537a543f8b5bc6
kalekale <kalekale.anon@gmail.com>
2021-05-16 16:52:14 +0545

    Clean up warnings and Properly close the gtk ui after video is over

--- src/gtk-ui.h
+++ src/gtk-ui.h
@@ -24,62 +24,17 @@ typedef struct _CustomData {
 
 void create_ui (CustomData *data);
 
-static void realize_cb (GtkWidget *widget, CustomData *data) {
-  GdkWindow *window = gtk_widget_get_window (widget);
-  guintptr window_handle;
+void realize_cb (GtkWidget *widget, CustomData *data);
 
-  if (!gdk_window_ensure_native (window))
-    g_error ("Couldn't create native window needed for GstVideoOverlay!");
+//static void play_cb (CustomData *data);
 
-  window_handle = GDK_WINDOW_XID (window);
-  
-  gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (data->playbin), window_handle);
-}
+//static void pause_cb (CustomData *data);
 
-// PLAY
-static void play_cb (CustomData *data) {
-  gst_element_set_state (data->playbin, GST_STATE_PLAYING);
-}
+void stop_cb (GtkButton *button, CustomData *data); 
 
-// PAUSE
-static void pause_cb (CustomData *data) {
-  gst_element_set_state (data->playbin, GST_STATE_PAUSED);
-}
+void delete_event_cb (GtkWidget *widget, GdkEvent *event, CustomData *data);
 
-// STOP
-static void stop_cb (GtkButton *button, CustomData *data) {
-  gst_element_set_state (data->playbin, GST_STATE_READY);
-}
+gboolean draw_cb (GtkWidget *widget, cairo_t *cr, CustomData *data);
 
-/* This function is called when the main window is closed */
-static void delete_event_cb (GtkWidget *widget, GdkEvent *event, CustomData *data) {
-  stop_cb (NULL, data);
-  gtk_main_quit ();
-}
-
-/* This function is called everytime the video window needs to be redrawn (due to damage/exposure,
- * rescaling, etc). GStreamer takes care of this in the PAUSED and PLAYING states, otherwise,
- * we simply draw a black rectangle to avoid garbage showing up. */
-static gboolean draw_cb (GtkWidget *widget, cairo_t *cr, CustomData *data) {
-  if (data->state < GST_STATE_PAUSED) {
-    GtkAllocation allocation;
-
-    /* Cairo is a 2D graphics library which we use here to clean the video window.
-     * It is used by GStreamer for other reasons, so it will always be available to us. */
-    gtk_widget_get_allocation (widget, &allocation);
-    cairo_set_source_rgb (cr, 0, 0, 0);
-    cairo_rectangle (cr, 0, 0, allocation.width, allocation.height);
-    cairo_fill (cr);
-  }
-
-  return FALSE;
-}
-
-/* This function is called when the slider changes its position. We perform a seek to the
- * new position here. */
-static void slider_cb (GtkRange *range, CustomData *data) {
-  gdouble value = gtk_range_get_value (GTK_RANGE (data->slider));
-  gst_element_seek_simple (data->playbin, GST_FORMAT_TIME, GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_KEY_UNIT,
-      (gint64)(value * GST_SECOND));
-}
+//static void slider_cb (GtkRange *range, CustomData *data);
 
